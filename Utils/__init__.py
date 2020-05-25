dda72d420d5b7834603a3dcd52b77d18186f3e6e
dawidejdeholm <dawid@ejdeholm.eu>
2020-05-26 00:02:21 +0200

    finalizing BAD

--- Utils/__init__.py
+++ Utils/__init__.py
@@ -2,3 +2,5 @@ from .utils import *
 from .SECParser import *
 from .MANIAC import *
 from .config import *
+from .bad_utils import *
+from .BAD import *
--- Utils/config.py
+++ Utils/config.py
@@ -1,6 +1,11 @@
 import Utils.utils as util
 from easydict import EasyDict as edict
 
+"""
+    SETTINGS FOR MANIAC DATASET
+
+"""
+
 def get_maniac_cfg():
 
     cfg = edict()
@@ -37,6 +42,10 @@ def get_maniac_cfg():
 
     return cfg
 
+"""
+    SETTINGS FOR BIMANUAL ACTION DATASET
+
+"""
 def get_bac_cfg():
 
     cfg = edict()
@@ -48,8 +57,12 @@ def get_bac_cfg():
     cfg.spatial_map = ['above', 'behind of', 'below', 'contact', 'fixed moving together', 'getting close',
                        'halting together', 'in front of', 'inside', 'left of', 'moving apart', 'moving together',
                        'right of', 'stable', 'surround', 'temporal']
-    cfg.objects     = ['LeftHand', 'RightHand', 'banana', 'bottle', 'bowl', 'cereals', 'cup', 'cuttingboard', 
+    cfg.objects     = ['LeftHand', 'RightHand', 'banana', 'bottle', 'bowl', 'cereals', 'cup', 'cuttingboard',
                        'hammer', 'harddrive', 'knife', 'saw', 'screwdriver', 'sponge', 'whisk', 'woodenwedge']
+
+    cfg.original_index = ['idle', 'approach', 'retreat', 'lift', 'place', 'hold', 'pour', 'cut',
+                          'hammer', 'saw', 'stir', 'screw', 'drink', 'wipe']
+
 
     # Creates one hot encodings for actions, relations and objects.
     cfg._relations = util.one_hot_string(cfg.spatial_map).tolist()
@@ -57,14 +70,14 @@ def get_bac_cfg():
     cfg._objects = util.one_hot_string(cfg.objects).tolist()
 
     # Time window
-    cfg.time_window = 4
-    cfg.temporal_graphs = True
+    cfg.time_window = 1
+    cfg.temporal_graphs = False
+    cfg.seperate_samples = False
 
-    cfg.skip_connections = False
     cfg.summery_writer = False
 
     # MODEL CONFIG
-    cfg.batch_size = 32
+    cfg.batch_size = 64
     cfg.learning_rate = 0.001
     cfg.dropout = 0.2
 
